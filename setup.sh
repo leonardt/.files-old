987855b6c7a99559d058c298ba14ae1ba336b0d5
Leonard Truong <leonardtruong@gmail.com>
2015-09-25 09:04:28 -0700

    vim bootstrap

--- setup.sh
+++ setup.sh
@@ -1,4 +1,13 @@
 #! /bin/bash
 
-ln -s $HOME/.files/vimrc $HOME/.vimrc
-ln -s $HOME/.files/vimrc $HOME/.nvimrc
+mkdir `pwd`/.vim
+ln -s `pwd`/.vim $HOME/.vim
+ln -s `pwd`/.vim $HOME/.nvim
+
+curl -fLo `pwd`/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+ln -s `pwd`/.files/vimrc $HOME/.vimrc
+ln -s `pwd`/.files/vimrc $HOME/.nvimrc
+
+vim -c "set et|PlugInstall|wqa"
