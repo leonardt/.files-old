67a3cafe79f1683188ed4ff87e42d20d335062bd
Leonard Truong <leonardtruong@gmail.com>
2015-09-25 09:30:23 -0700

    Fix symlink source

--- setup.sh
+++ setup.sh
@@ -7,8 +7,8 @@ ln -s `pwd`/.vim $HOME/.nvim
 curl -fLo `pwd`/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-ln -s `pwd`/.files/vimrc $HOME/.vimrc
-ln -s `pwd`/.files/vimrc $HOME/.nvimrc
+ln -s `pwd`/vimrc $HOME/.vimrc
+ln -s `pwd`/vimrc $HOME/.nvimrc
 
 if hash nvim 2>/dev/null; then
     nvim -c "PlugInstall|wqa"
